5a8049994e332b36977e87238de5e9420dc51340
osic <fortyone@live.at>
2025-11-28 11:41:41 +0100

    [nvim]: feat(lsp): add initial LSP configuration plugin
    - Introduce lua/plugins/lsp.lua with nvim-lspconfig setup
    - Define server table for yaml, json, lua, vtsls, and pyright

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      yamlls = {},
+      jsonls = {},
+      lua_ls = {},
+      vtsls = {},
+      pyright = {},
+    },
+  },
+}
